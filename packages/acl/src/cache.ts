dc5fc576653aa27bc0b4c2581c49e9e494e62066
Junmin Ahn <junminahn@outlook.com>
2023-09-19 21:18:57 -0700

    chore: rename cache file

---
+++ packages/acl/src/cache.ts
@@ -0,0 +1,39 @@
+export class Cache<K, V> {
+  private cache: Map<K, V>;
+
+  constructor() {
+    this.cache = new Map();
+  }
+
+  set(key: K, value: V): void {
+    this.cache.set(key, value);
+  }
+
+  get(key: K): V | undefined {
+    return this.cache.get(key);
+  }
+
+  delete(key: K): void {
+    this.cache.delete(key);
+  }
+
+  clear(): void {
+    this.cache.clear();
+  }
+
+  has(key: K): boolean {
+    return this.cache.has(key);
+  }
+
+  keys(): K[] {
+    return Array.from(this.cache.keys());
+  }
+
+  values(): V[] {
+    return Array.from(this.cache.values());
+  }
+
+  size(): number {
+    return this.cache.size;
+  }
+}
