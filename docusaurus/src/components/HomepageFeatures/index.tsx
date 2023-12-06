e436f28730a1a3b744c9669371d747035ba25ad9
j.dev <junmin.dev@outlook.com>
2023-12-05 21:10:35 -0800

    docs: update main pillars

--- docusaurus/src/components/HomepageFeatures/index.tsx
+++ docusaurus/src/components/HomepageFeatures/index.tsx
@@ -10,38 +10,38 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Enhanced Backend Security',
+    Svg: require('@site/static/img/security.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Implement robust access controls and ACLs to secure backend databases, ensuring restricted access to sensitive
+        information.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Optimized Frontend Communication',
+    Svg: require('@site/static/img/communication.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Improve frontend-backend communication through a dedicated client adapter, streamlining interactions with
+        backend API endpoints.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Simplified Authorization Logic',
+    Svg: require('@site/static/img/portfolio.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Enhance code organization and readability with TypeScript decorators, simplifying the definition of
+        configurations and options for improved authorization logic.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
